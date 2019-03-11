92e4c72f402c5a9919a919f39405d265a6916693
msc <Michael.Schaidnagel@smart-digital.de>
2019-03-11 21:46:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn General Kenobiwan
           </a>
         </header>
       </div>
